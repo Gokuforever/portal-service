51d9dda219f469356c85afea91d2c6360b6a8055
rajra <rajra@LAPTOP-V28VVQMP>
2024-09-07 17:41:40 +0530

    Edit seller api changes

--- src/main/java/com/sorted/portal/bl_services/ManageSeller_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageSeller_BLService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -260,6 +263,154 @@ public class ManageSeller_BLService {
 		}
 	}
 
+	@PostMapping("/edit")
+	public SEResponse edit(@RequestBody SERequest request, HttpServletRequest servletRequest) {
+		try {
+			log.info("/seller/edit:: API started!");
+			CUDSellerBean req = request.getGenericRequestDataObject(CUDSellerBean.class);
+			CommonUtils.extractHeaders(servletRequest, req);
+
+			UsersBean usersBean = users_Service.validateUserForActivity(req, Permission.EDIT,
+					Activity.SELLER_ONBOARDING);
+			if (!UserType.SUPER_ADMIN.equals(usersBean.getRole().getUser_type())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.ACCESS_DENIED);
+			}
+			if (!StringUtils.hasText(req.getSeller_id())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.SELLER_ID_MANDATE);
+			}
+			Address address = validateSellerReq(req);
+
+			SEFilter filterDbS = new SEFilter(SEFilterType.AND);
+			filterDbS.addClause(WhereClause.eq(BaseMongoEntity.Fields.id, req.getSeller_id()));
+			filterDbS.addClause(WhereClause.eq(BaseMongoEntity.Fields.deleted, false));
+			Seller seller = seller_Service.repoFindOne(filterDbS);
+			if (seller == null) {
+				throw new CustomIllegalArgumentsException(ResponseCode.SELLER_NOT_FOUND);
+			}
+			Optional<Spoc_Details> result = seller.getSpoc_details().stream().filter(Spoc_Details::isPrimary)
+					.findFirst();
+			int db_spocHash = getSpocHashCode(result.get());
+
+			List<Spoc_Details> spoc_details = req.getSpoc_details();
+			Spoc_Details primary_spoc = validateSPOC(spoc_details);
+			int req_spocHash = getSpocHashCode(primary_spoc);
+
+			if (db_spocHash != req_spocHash) {
+
+				List<String> unique_phone = spoc_details.parallelStream().map(Spoc_Details::getMobile_no).distinct()
+						.collect(Collectors.toList());
+
+				SEFilter filterS = new SEFilter(SEFilterType.AND);
+				filterS.addClause(WhereClause.eq(BaseMongoEntity.Fields.deleted, false));
+				filterS.addClause(WhereClause.notEq(BaseMongoEntity.Fields.id, seller.getId()));
+				Map<String, Object> map = new HashMap<>();
+				map.put(Spoc_Details.Fields.mobile_no, unique_phone);
+				map.put(Spoc_Details.Fields.primary, true);
+				filterS.addClause(WhereClause.elem_match(Seller.Fields.spoc_details, map));
+				Seller temp_seller = seller_Service.repoFindOne(filterS);
+				if (temp_seller != null) {
+					throw new CustomIllegalArgumentsException(ResponseCode.DUPLICATE_SE_MOBILE);
+				}
+
+				SEFilter filterR = new SEFilter(SEFilterType.AND);
+				filterR.addClause(WhereClause.eq(Role.Fields.seller_id, seller.getId()));
+				filterR.addClause(WhereClause.eq(Role.Fields.user_type, UserType.SELLER.name()));
+				Role role = roleService.repoFindOne(filterR);
+				if (role == null) {
+					throw new CustomIllegalArgumentsException(ResponseCode.ROLE_MISSING);
+				}
+
+				/*
+				 * fetching user details on the basis of role as there is One Role and One
+				 * User per Seller
+				 */
+
+				SEFilter filterU = new SEFilter(SEFilterType.AND);
+				filterU.addClause(WhereClause.eq(Users.Fields.mobile_no, primary_spoc.getMobile_no()));
+				filterU.addClause(WhereClause.notEq(Users.Fields.role_id, role.getId()));
+				filterU.addClause(WhereClause.eq(BaseMongoEntity.Fields.deleted, false));
+				long duplicate = users_Service.countByFilter(filterU);
+				if (duplicate > 0) {
+					throw new CustomIllegalArgumentsException(ResponseCode.DUPLICATE_USER_MOBILE);
+				}
+				filterU = new SEFilter(SEFilterType.AND);
+				filterU.addClause(WhereClause.eq(Users.Fields.email_id, primary_spoc.getEmail_id()));
+				filterU.addClause(WhereClause.notEq(Users.Fields.role_id, role.getId()));
+				filterU.addClause(WhereClause.eq(BaseMongoEntity.Fields.deleted, false));
+				duplicate = users_Service.countByFilter(filterU);
+				if (duplicate > 0) {
+					throw new CustomIllegalArgumentsException(ResponseCode.DUPLICATE_USER_EMAIL);
+				}
+
+				filterU = new SEFilter(SEFilterType.AND);
+				filterU.addClause(WhereClause.eq(Users.Fields.role_id, role.getId()));
+				Users user = users_Service.repoFindOne(filterU);
+				if (user == null) {
+					throw new CustomIllegalArgumentsException(ResponseCode.USER_NOT_FOUND);
+				}
+
+				user.setFirst_name(primary_spoc.getFirst_name());
+				user.setLast_name(primary_spoc.getLast_name());
+				user.setMobile_no(primary_spoc.getMobile_no());
+				user.setEmail_id(primary_spoc.getEmail_id());
+//				String generatePassword = PasswordValidatorUtils.generatePassword();
+//				String encode = passwordEncoder.encode(generatePassword);
+//				user.setPassword(encode);
+				users_Service.update(user.getId(), user, usersBean.getId());
+
+			}
+
+			Bank_Details bank_details = req.getBank_details();
+			if (bank_details == null) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_BANK_DETAILS);
+			}
+			ValidationUtil.validateBankDetails(bank_details);
+			List<String> serviceable_pincodes = req.getServiceable_pincodes();
+			if (CollectionUtils.isEmpty(serviceable_pincodes)) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SERVICEABLE_PINCODE);
+			}
+			for (String pincode : serviceable_pincodes) {
+				if (!SERegExpUtils.isPincode(pincode)) {
+					throw new CustomIllegalArgumentsException(ResponseCode.INVALID_PINCODE);
+				}
+			}
+
+			String name = CommonUtils.toTitleCase(req.getName());
+			String pan_no = req.getPan_no();
+			seller.setBusiness_name(name);
+			seller.setSpoc_details(spoc_details);
+			seller.setServiceable_pincodes(serviceable_pincodes);
+			seller.setCompany_pan(pan_no);
+			seller.setBank_details(bank_details);
+			seller_Service.update(seller.getId(), seller, usersBean.getId());
+
+			SEFilter filterA = new SEFilter(SEFilterType.AND);
+			filterA.addClause(WhereClause.eq(Address.Fields.entity_id, seller.getId()));
+			filterA.addClause(WhereClause.eq(Address.Fields.user_type, UserType.SELLER.name()));
+			Address dbAddress = address_Service.repoFindOne(filterA);
+			if (dbAddress == null) {
+				throw new CustomIllegalArgumentsException(ResponseCode.ADDRESS_NOT_FOUND);
+			}
+			int db_hash = getSellerAddresshashCode(dbAddress);
+
+			address.setUser_type(UserType.SELLER);
+			address.setEntity_id(seller.getId());
+			int req_hash = getSellerAddresshashCode(address);
+			if (db_hash != req_hash) {
+				//FIXME: creating multiple entries in update
+//				address_Service.update(dbAddress.getId(), address, req.getReq_user_id());
+			}
+			log.info("/seller/edit:: API ended");
+			return SEResponse.getEmptySuccessResponse(ResponseCode.SUCCESSFUL);
+		} catch (CustomIllegalArgumentsException ex) {
+			throw ex;
+		} catch (Exception e) {
+			log.error("/seller/edit:: exception occurred");
+			log.error("/seller/edit:: {}", e.getMessage());
+			throw new CustomIllegalArgumentsException(ResponseCode.ERR_0001);
+		}
+	}
+
 	@PostMapping("/changeStatus")
 	public SEResponse changeStatus(@RequestBody SERequest request, HttpServletRequest servletRequest) {
 		try {
@@ -374,4 +525,88 @@ public class ManageSeller_BLService {
 			throw new CustomIllegalArgumentsException(ResponseCode.ERR_0001);
 		}
 	}
+
+	private Address validateSellerReq(CUDSellerBean req) {
+		if (!StringUtils.hasText(req.getName())) {
+			throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SELLER_NAME);
+		}
+		if (!SERegExpUtils.standardTextValidation(req.getName())) {
+			throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SELLER_NAME);
+		}
+		if (!StringUtils.hasText(req.getPan_no())) {
+			throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_PAN_NO);
+		}
+		if (!SERegExpUtils.isPan(req.getPan_no())) {
+			throw new CustomIllegalArgumentsException(ResponseCode.INVALID_PAN_NO);
+		}
+		AddressDTO addressDTO = req.getAddress();
+		if (addressDTO == null) {
+			throw new CustomIllegalArgumentsException(ResponseCode.MISSING_ADDRESS);
+		}
+		Address address = ValidationUtil.validateAddress(addressDTO);
+		if (CollectionUtils.isEmpty(req.getSpoc_details())) {
+			throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SPOC);
+		}
+		return address;
+	}
+
+	private Spoc_Details validateSPOC(List<Spoc_Details> spoc_details) {
+		List<String> unique_phone = new ArrayList<>();
+		Spoc_Details primary_spoc = null;
+		for (Spoc_Details spoc : spoc_details) {
+			if (!StringUtils.hasText(spoc.getFirst_name())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SPOC_FNAME);
+			}
+			if (!SERegExpUtils.standardTextValidation(spoc.getFirst_name())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.INVALID_SPOC_FNAME);
+			}
+			if (!StringUtils.hasText(spoc.getLast_name())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SPOC_LNAME);
+			}
+			if (!SERegExpUtils.standardTextValidation(spoc.getLast_name())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.INVALID_SPOC_LNAME);
+			}
+			if (!StringUtils.hasText(spoc.getEmail_id())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SPOC_MAIL);
+			}
+			if (!SERegExpUtils.isEmail(spoc.getEmail_id())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.INVALID_SPOC_MAIL);
+			}
+			if (!StringUtils.hasText(spoc.getMobile_no())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SPOC_PHONE);
+			}
+			if (!SERegExpUtils.isMobileNo(spoc.getMobile_no())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.INVALID_SPOC_PHONE);
+			}
+			if (!StringUtils.hasText(spoc.getDesignation())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.MANDATE_SPOC_DESIGNATION);
+			}
+			if (!SERegExpUtils.standardTextValidation(spoc.getDesignation())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.INVALID_SPOC_DESIGNATION);
+			}
+			if (unique_phone.contains(spoc.getMobile_no())) {
+				throw new CustomIllegalArgumentsException(ResponseCode.DUPLICATE_SPOC_PHONE);
+			}
+			unique_phone.add(spoc.getMobile_no());
+			if (spoc.isPrimary() && primary_spoc == null) {
+				primary_spoc = spoc;
+			} else {
+				spoc.setPrimary(false);
+			}
+		}
+		if (primary_spoc == null) {
+			throw new CustomIllegalArgumentsException(ResponseCode.MARK_PRIMARY);
+		}
+		return primary_spoc;
+	}
+
+	private int getSpocHashCode(Spoc_Details s) {
+		return Objects.hash(s.getFirst_name(), s.getLast_name(), s.getMobile_no(), s.getEmail_id());
+	}
+
+	private int getSellerAddresshashCode(Address a) {
+		return Objects.hash(a.getStreet_1(), a.getStreet_2(), a.getLandmark(), a.getCity(), a.getState(),
+				a.getPincode(), a.getAddress_type(), a.getAddress_type_desc());
+	}
+
 }
