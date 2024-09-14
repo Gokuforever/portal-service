82b57ccd727805081c03daf3f3d66306a8b6ab32
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-09-14 05:33:48 +0530

    ProductDetailsBean:: new fields added

--- src/main/java/com/sorted/portal/assisting/beans/ProductDetailsBean.java
+++ src/main/java/com/sorted/portal/assisting/beans/ProductDetailsBean.java
@@ -3,6 +3,8 @@ package com.sorted.portal.assisting.beans;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.sorted.commons.beans.Media;
+
 import lombok.Data;
 
 @Data
@@ -11,10 +13,11 @@ public class ProductDetailsBean {
 	private String name;
 	private String id;
 	private String product_code;
+	private BigDecimal mrp;
 	private BigDecimal selling_price;
-	private String img_url;
 	private String description;
 	private List<ProductDetailsBean> varients;
 	private List<ProductDetailsBean> related_products;
+	private List<Media> media;
 	private boolean is_deliverable;
 }
