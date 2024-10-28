c59fd6cec671fad264731d6730e2bba64f3a2427
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2024-10-28 22:04:46 +0530

    ManageFileUpload_BLService:: download API added

--- src/main/java/com/sorted/portal/bl_services/ManageFileUpload_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageFileUpload_BLService.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sorted.commons.enums.ResponseCode;
 import com.sorted.commons.helper.SEResponse;
 import com.sorted.commons.utils.GoogleDriveService;
 
@@ -16,31 +17,9 @@ public class ManageFileUpload_BLService {
 		this.googleDriveService = googleDriveService;
 	}
 
-//	@Autowired
-//	private Users_Service users_Service;
-
-//	@PostMapping("/uploadfile")
-//	public SEResponse uploadPhoto(@RequestParam("file") MultipartFile file, HttpServletRequest httpServletRequest) {
-//		try {
-//			String req_user_id = httpServletRequest.getHeader("req_user_id").toString();
-//			String req_role_id = httpServletRequest.getHeader("req_role_id").toString();
-//			if (!StringUtils.hasText(req_user_id) || !StringUtils.hasText(req_role_id)) {
-//				throw new CustomIllegalArgumentsException(ResponseCode.ACCESS_DENIED);
-//			}
-//			UsersBean usersBean = users_Service.validateUserForActivity(req_user_id, Activity.INVENTORY_MANAGEMENT);
-//			String fileId = googleDriveService.uploadPhoto(file, usersBean, DocumentType.PRODUCT_IMAGE);
-//			return SEResponse.getBasicSuccessResponseObject(fileId, ResponseCode.SUCCESSFUL);
-//		} catch (IOException | GeneralSecurityException e) {
-//			e.printStackTrace();
-//			return SEResponse.getBadRequestFailureResponse(ResponseCode.ERR_0001);
-//		}
-//	}
-
 	@GetMapping("/download")
-	public SEResponse fetchPhoto(@RequestParam("fileId") String fileId,
-			@RequestParam("destinationPath") String destinationPath) {
-		googleDriveService.fetchPhoto(fileId, destinationPath);
-		return SEResponse.getBasicSuccessResponseObject(fileId, null);
-
+	public SEResponse fetchPhoto(@RequestParam("fileId") String fileId) throws Exception {
+		String fetchPhoto = googleDriveService.fetchPhoto(fileId);
+		return SEResponse.getBasicSuccessResponseObject(fetchPhoto, ResponseCode.SUCCESSFUL);
 	}
 }
