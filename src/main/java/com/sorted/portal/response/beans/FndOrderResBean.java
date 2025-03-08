246114f45ff6c458506139d65abcc205bd2dda96
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-03-08 15:09:33 +0530

    FndOrderResBean:: new bean added

--- src/main/java/com/sorted/portal/response/beans/FndOrderResBean.java
+++ src/main/java/com/sorted/portal/response/beans/FndOrderResBean.java
@@ -1,20 +1,23 @@
 package com.sorted.portal.response.beans;
 
 import com.sorted.commons.beans.AddressDTO;
-
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Builder
 @Getter
 public class FndOrderResBean {
 
-	private String id;
-	private String code;
-	private Long total_amount;
-	private String status;
-	private String transaction_id;
-	private AddressDTO delivery_address;
-	private AddressDTO pickup_address;
+    private String id;
+    private String code;
+    private Long total_amount;
+    private String status;
+    private String transaction_id;
+    private AddressDTO delivery_address;
+    private AddressDTO pickup_address;
+    private List<OrderItemDTO> orderItems;
+    private String creation_date_str;
 
 }
