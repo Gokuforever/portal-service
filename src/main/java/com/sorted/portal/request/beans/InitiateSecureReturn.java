f623102c571a8ab2997c7a2254b60671e98f20cf
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-04-10 13:45:30 +0530

    Major push dump

---
+++ src/main/java/com/sorted/portal/request/beans/InitiateSecureReturn.java
@@ -0,0 +1,13 @@
+package com.sorted.portal.request.beans;
+
+import com.sorted.commons.helper.ReqBaseBean;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class InitiateSecureReturn extends ReqBaseBean {
+	private String order_id;
+
+}
---
+++ src/main/java/com/sorted/portal/security/PortalServiceInterceptorAppConfig.java
@@ -0,0 +1,17 @@
+package com.sorted.portal.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class PortalServiceInterceptorAppConfig implements WebMvcConfigurer {
+	@Autowired
+	PortalServiceInterceptor portalServiceInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(portalServiceInterceptor).excludePathPatterns("/**");
+	}
+}
