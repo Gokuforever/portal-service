374bd52554837645d3a525d7f3e6a678200d7ab3
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-08-25 02:50:52 +0530

    Cart addition issue fixed

--- src/main/java/com/sorted/portal/bl_services/ManageCart_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageCart_BLService.java
@@ -159,11 +159,10 @@ public class ManageCart_BLService {
 
             List<Item> listItems = new ArrayList<>();
             if (itemBean.getQuantity() == 0) {
-                Predicate<Item> p1 = x -> !x.getProduct_id().equals(itemBean.getProduct_id());
-                Predicate<Item> p2 = x -> x.is_secure() != itemBean.isSecure_item();
-                List<Item> cart_items = cart.getCart_items().stream().filter(p1.and(p2)).toList();
-                if (!CollectionUtils.isEmpty(cart_items)) {
-                    listItems.addAll(cart_items);
+                for (Item cartItem : cart.getCart_items()) {
+                    if (!cartItem.getProduct_id().equals(itemBean.getProduct_id()) || (cartItem.getProduct_id().equals(itemBean.getProduct_id()) && cartItem.is_secure() != itemBean.isSecure_item())) {
+                        listItems.add(cartItem);
+                    }
                 }
             } else {
 
