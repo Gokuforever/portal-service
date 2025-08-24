f570344adb4eb060a30c37680a2465e171f14abe
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-08-24 17:10:05 +0530

    Minor change

--- src/main/java/com/sorted/portal/bl_services/ManageStoreActivity_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageStoreActivity_BLService.java
@@ -1,32 +1,70 @@
 package com.sorted.portal.bl_services;
 
 import com.sorted.commons.beans.UsersBean;
+import com.sorted.commons.entity.mongo.Order_Details;
+import com.sorted.commons.entity.mongo.Order_Item;
+import com.sorted.commons.entity.service.Order_Details_Service;
+import com.sorted.commons.entity.service.Order_Item_Service;
 import com.sorted.commons.entity.service.StoreActivityService;
 import com.sorted.commons.entity.service.Users_Service;
 import com.sorted.commons.enums.Activity;
+import com.sorted.commons.enums.OrderStatus;
 import com.sorted.commons.enums.ResponseCode;
 import com.sorted.commons.enums.UserType;
 import com.sorted.commons.exceptions.CustomIllegalArgumentsException;
+import com.sorted.commons.helper.AggregationFilter.*;
 import com.sorted.commons.helper.SEResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class ManageStoreActivity_BLService {
 
     private final StoreActivityService storeActivityService;
+    private final Order_Details_Service orderDetailsService;
+    private final Order_Item_Service orderItemService;
     private final Users_Service users_Service;
 
     @PostMapping("/store/open")
     public SEResponse openStore(HttpServletRequest request) {
         UsersBean usersBean = validateUserForActivity(request, Activity.OPEN_STORE);
         storeActivityService.openStore(usersBean.getSeller().getId(), usersBean.getId());
+        SEFilter filter = new SEFilter(SEFilterType.AND);
+        filter.addClause(WhereClause.eq(Order_Details.Fields.seller_id, usersBean.getSeller().getId()));
+        filter.addClause(WhereClause.eq(Order_Details.Fields.status_id, OrderStatus.STORE_NOT_OPERATIONAL.getId()));
+
+        List<Order_Details> orderDetails = orderDetailsService.repoFind(filter);
+        if (!CollectionUtils.isEmpty(orderDetails)) {
+
+            SEFilter filterOI = new SEFilter(SEFilterType.AND);
+            filterOI.addClause(WhereClause.eq(Order_Item.Fields.order_id, orderDetails.stream().map(Order_Details::getId).toList()));
+
+            List<Order_Item> orderItems = orderItemService.repoFind(filterOI);
+            Map<String, List<Order_Item>> orderItemMap = orderItems.stream().collect(Collectors.groupingBy(Order_Item::getOrder_id));
+
+            for (Order_Details orderDetail : orderDetails) {
+                orderDetail.setStatus(OrderStatus.TRANSACTION_PROCESSED, usersBean.getId());
+                orderDetailsService.update(orderDetail.getId(), orderDetail, usersBean.getId());
+
+                List<Order_Item> orderItemList = orderItemMap.get(orderDetail.getId());
+                for (Order_Item orderItem : orderItemList) {
+                    orderItem.setStatus(OrderStatus.TRANSACTION_PROCESSED, usersBean.getId());
+                    orderItemService.update(orderItem.getId(), orderItem, usersBean.getId());
+                }
+            }
+        }
         return SEResponse.getEmptySuccessResponse(ResponseCode.SUCCESSFUL);
     }
 
