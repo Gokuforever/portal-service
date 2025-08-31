400e062e230ae29b44d101d836151989a1112501
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-08-31 15:58:55 +0530

    New API added for fetching verified users

--- src/main/java/com/sorted/portal/bl_services/ManageUsers_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageUsers_BLService.java
@@ -1,8 +1,11 @@
 package com.sorted.portal.bl_services;
 
 import com.sorted.commons.entity.mongo.BaseMongoEntity;
+import com.sorted.commons.entity.mongo.Role;
 import com.sorted.commons.entity.mongo.Users;
+import com.sorted.commons.entity.service.RoleService;
 import com.sorted.commons.entity.service.Users_Service;
+import com.sorted.commons.enums.UserType;
 import com.sorted.commons.helper.AggregationFilter.*;
 import com.sorted.portal.response.beans.VerifiedUsersResBean;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +20,24 @@ import java.util.List;
 public class ManageUsers_BLService {
 
     private final Users_Service usersService;
+    private final RoleService roleService;
 
 
 
     @GetMapping("/verified/users")
     public List<VerifiedUsersResBean> getAllVerifiedUsers(){
 
+        SEFilter filterR = new SEFilter(SEFilterType.AND);
+        filterR.addClause(WhereClause.eq(BaseMongoEntity.Fields.deleted, false));
+        filterR.addClause(WhereClause.eq(Role.Fields.user_type_id, UserType.CUSTOMER.getId()));
+
+        Role role = roleService.repoFindOne(filterR);
+
+
         SEFilter filter = new SEFilter(SEFilterType.AND);
         filter.addClause(WhereClause.eq(BaseMongoEntity.Fields.deleted, false));
         filter.addClause(WhereClause.eq(Users.Fields.is_verified, true));
+        filter.addClause(WhereClause.eq(Users.Fields.role_id, role.getId()));
         List<Users> users = usersService.repoFind(filter);
 
         List<VerifiedUsersResBean> verifiedUsersResBeans = new ArrayList<>();
