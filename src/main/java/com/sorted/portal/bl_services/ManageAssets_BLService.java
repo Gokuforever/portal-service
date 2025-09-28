94de7be0ae088d3c2fa5b7899316cc635a2298c8
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-09-28 13:32:44 +0530

    Log added

--- src/main/java/com/sorted/portal/bl_services/ManageAssets_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageAssets_BLService.java
@@ -9,6 +9,7 @@ import com.sorted.commons.helper.AggregationFilter.*;
 import com.sorted.commons.utils.AwsS3Service;
 import com.sorted.portal.request.beans.UploadAssetBean;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 
+@Log4j2
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/assets")
@@ -27,6 +29,9 @@ public class ManageAssets_BLService {
 
     @PostMapping("/promo-banner/create")
     public void promoBannerCreate(@RequestBody UploadAssetBean request) throws IOException {
+
+        log.info("Request: {}", request);
+
         String cdnUrl = awsS3Service.uploadPhoto(request.getBytes(), request.getContentType(), request.getFileName());
 
         int existingOrder = 0;
