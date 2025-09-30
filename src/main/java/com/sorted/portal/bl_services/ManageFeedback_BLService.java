31a42334b587267447488fc50fff4c91be99a211
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2025-10-01 02:28:07 +0530

    Minor change

--- src/main/java/com/sorted/portal/bl_services/ManageFeedback_BLService.java
+++ src/main/java/com/sorted/portal/bl_services/ManageFeedback_BLService.java
@@ -3,6 +3,7 @@ package com.sorted.portal.bl_services;
 import com.sorted.commons.constants.Defaults;
 import com.sorted.commons.entity.mongo.Feedback;
 import com.sorted.commons.entity.service.FeedbackService;
+import com.sorted.portal.request.beans.FeedbackBean;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,12 +17,12 @@ public class ManageFeedback_BLService {
     private final FeedbackService feedbackService;
 
     @PostMapping("/feedback")
-    public String addFeedback(@RequestBody String feedback, HttpServletRequest request) {
+    public String addFeedback(@RequestBody FeedbackBean feedback, HttpServletRequest request) {
 
         String req_user_id = request.getHeader("req_user_id");
 
         Feedback build = Feedback.builder()
-                .feedback(feedback)
+                .feedback(feedback.getFeedback())
                 .userId(req_user_id)
                 .build();
 
---
+++ src/main/java/com/sorted/portal/request/beans/FeedbackBean.java
@@ -0,0 +1,12 @@
+package com.sorted.portal.request.beans;
+
+import com.sorted.commons.helper.ReqBaseBean;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class FeedbackBean extends ReqBaseBean {
+
+    private String feedback;
+}
