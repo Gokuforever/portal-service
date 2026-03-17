84e6651f2379a24e006c98f252e2b698515c0f66
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2026-03-17 23:11:12 +0530

    Initiate secure pick up cron activated

--- portal-service/src/main/java/com/sorted/portal/crons/InitiateSecurePickUpCron.java
+++ portal-service/src/main/java/com/sorted/portal/crons/InitiateSecurePickUpCron.java
@@ -3,6 +3,7 @@ package com.sorted.portal.crons;
 import com.sorted.portal.service.secure.SecureReturnService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ public class InitiateSecurePickUpCron {
 
     private final SecureReturnService secureReturnService;
 
+    @Scheduled(fixedRate = 60000)
     public void initiateSecureReturns() {
         log.info("Initiating secure returns for orders.");
         secureReturnService.initiateSecurePickUp();
