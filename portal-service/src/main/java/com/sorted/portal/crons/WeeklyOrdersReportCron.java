d73034436cd6c61164a0003cd041566968ee67ad
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2026-03-20 00:40:07 +0530

    Partial refund changes handled

--- portal-service/src/main/java/com/sorted/portal/crons/WeeklyOrdersReportCron.java
+++ portal-service/src/main/java/com/sorted/portal/crons/WeeklyOrdersReportCron.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Component
+//@Component
 @Slf4j
 @RequiredArgsConstructor
 public class WeeklyOrdersReportCron {
